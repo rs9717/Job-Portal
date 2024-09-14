f2d3bb49fb24d41f4666483dbcc9c11cb55de57e
rohit <rohitsrivastava3000@gmail.com>
2024-09-14 19:19:56 +0530

    frontend and backend done

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import {
+  FaSquareXTwitter,
+  FaSquareInstagram,
+  FaYoutube,
+  FaLinkedin,
+} from "react-icons/fa6";
+
+const Footer = () => {
+  const { isAuthenticated } = useSelector((state) => state.user);
+  return (
+    <>
+      <div className="copyright">
+        &copy; CopyRight 2024. All Rights Reserved By XYZ Industries.
+      </div>
+    </>
+  );
+};
+
+export default Footer;
